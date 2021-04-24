0cca8b01a200f15d0aceea219d7dffa8cb251f68
jh Lim <jhwlim25@gmail.com>
2021-04-24 13:57:14 +0900

    파일 업로드 및 이미지 파일 crop & resize 업로드 유틸 추가

--- src/main/java/com/spring/fleamarket/global/util/file/FileConfig.java
+++ src/main/java/com/spring/fleamarket/global/util/file/FileConfig.java
@@ -1,5 +1,7 @@
 package com.spring.fleamarket.global.util.file;
 
+import java.io.File;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
@@ -8,7 +10,10 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @Configuration
 public class FileConfig {
 	
-	private static final int MAX_UPLOAD_SIZE = 10240000; // 10MB
+	private String root = "C:\\MyFleaMarket";
+	private String accountFileUploadPath = "\\account";
+	
+	private final int MAX_UPLOAD_SIZE = 10 * 1024 * 1024; // 10MB
 	
 	@Bean
 	public MultipartResolver filterMultipartResolver() {
@@ -17,6 +22,11 @@ public class FileConfig {
 		multipartResolver.setMaxUploadSize(MAX_UPLOAD_SIZE);
 		
 		return multipartResolver;
+	}
+	
+	@Bean(name = "accountFileUploadPath")
+	public String accountFileUploadPath() {
+		return root + File.separator + accountFileUploadPath;
 	}
 	
 }
---
+++ src/main/java/com/spring/fleamarket/global/util/file/FileUtils.java
@@ -0,0 +1,97 @@
+package com.spring.fleamarket.global.util.file;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+
+import org.imgscalr.Scalr;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.spring.fleamarket.global.util.file.exception.NotAcceptableImageFileFormatException;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class FileUtils {
+	
+	public static String uploadCroppedImageFile(MultipartFile file, String uploadPath, int x, int y, int width, int height, int resize) throws Exception {
+		String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+		String savedPath = getCurrentDayPath(uploadPath);
+		
+		File target = new File(savedPath, fileName);
+		BufferedImage src = ImageIO.read(file.getInputStream());
+		
+		String formatName = getFileFormatName(fileName);
+		if (!isImageFormat(formatName)) {
+			throw new NotAcceptableImageFileFormatException();
+		}
+		
+		BufferedImage croppedImage = Scalr.crop(src, x, y, width, height);
+		BufferedImage savedImage = Scalr.resize(croppedImage, resize);
+		
+		ImageIO.write(savedImage, formatName, target);
+	
+		log.info("File upload success");
+		return savedPath + File.separator + fileName;
+	}
+	
+	public static String uploadFile(MultipartFile file, String uploadPath) {
+		String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+		String savedPath = getCurrentDayPath(uploadPath);
+		File target = new File(savedPath, fileName);
+		
+		try {
+			FileCopyUtils.copy(file.getBytes(), target);
+		} catch (IOException e) {
+			log.warn("File upload fail");
+		}
+		
+		return savedPath + File.separator + fileName;
+	}
+	
+	private static String getCurrentDayPath(String uploadRootPath) {
+		Calendar currentDay = Calendar.getInstance();
+		
+		int year = currentDay.get(Calendar.YEAR);
+		int month = currentDay.get(Calendar.MONTH) + 1;
+		int day = currentDay.get(Calendar.DATE);
+		
+		return makeDir(uploadRootPath, 
+					   String.valueOf(year), 
+					   String.format("%02d", month),
+					   String.format("%02d", day));
+	}
+	
+	private static String makeDir(String uploadRootPath, String... paths) {
+		StringBuilder sb = new StringBuilder(uploadRootPath);
+		for (String path : paths) {
+			sb.append(File.separator + path);
+			File tmp = new File(sb.toString());
+			if (!tmp.exists()) {
+				tmp.mkdir();
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	private static String getFileFormatName(String fileName) {
+		int index = fileName.lastIndexOf(".");
+		return fileName.substring(index+1).toUpperCase();
+	}
+	
+	private static boolean isImageFormat(String fileFormat) {
+		for (ImageFormat format : ImageFormat.values()) {
+			if (format.name().equals(fileFormat)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
