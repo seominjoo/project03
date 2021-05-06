b5dae178d6bacb945216f14b4b2db319a9435482
jh Lim <jhwlim25@gmail.com>
2021-05-06 12:44:29 +0900

    reset.css 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/image/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -12,13 +12,6 @@ export default {
 }
 </script>
 
+<style src="./assets/css/reset.css"></style>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
